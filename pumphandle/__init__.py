d86fe174b36a00cb1bcf27f78caaf10456d3e238
elofgren <Eric.Lofgren@unc.edu>
2013-10-16 16:09:54 -0400

    Checking and validation
    Added functionality to check if file already exists - avoids overwrites
    and unnecessary downloads for large data files.

--- pumphandle/__init__.py
+++ pumphandle/__init__.py
@@ -1,9 +1,29 @@
 def NetDrop(url,filename):
+	'''Simple function to check for existence of data file and download file from internet 		source if not otherwise present'''
 	from requests import get
 	import os
-	pmlfile = get(url, verify=False)
-	pmlout = os.path.join(os.getcwd(),filename)
-	f = open(pmlout,'w')
-	f.write(pmlfile.content)
-	f.close()
+	fullname = os.path.join(os.getcwd(),filename)
+	if os.path.isfile(fullname):
+		existswarn = raw_input("File already exists. Overwrite (Y/N)? ")
+		existswarn = existswarn.lower()
+
+		while existswarn not in ['y','n']:
+			print "Invalid input, please try again."
+			existswarn = raw_input("File already exists. Overwrite (Y/N)? ")
+			existswarn = existswarn.lower()
+
+		if existswarn == "y":
+			netfile = get(url, verify=False)
+			f = open(fullname,'w')
+			f.write(netfile.content)
+			f.close()
+			
+		elif existswarn =="n":
+			print "Download canceled"
+			
+	else:
+		netfile = get(url, verify=False)
+		f = open(fullname,'w')
+		f.write(netfile.content)
+		f.close()
 	return()
