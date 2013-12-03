82778ea723fcd41ac3f9e34d26e112ec9fad3ce4
Eric Lofgren <lofgrene@vbi.vt.edu>
2013-12-03 15:32:51 -0500

    Code cleaning
    Cleaning up NetDrop with Flake8

--- README.rst
+++ README.rst
@@ -17,13 +17,20 @@ requests
 
 Versions
 --------
-0.0.1a - Initial PyPi release version.
-
+0.0.1 - Initial PyPi release version.
+0.0.2 - Minor changes to NetDrop code.
 
 Version Notes
 -------------
-0.0.1a - This release is intended primarily to make the developers life slightly easier by packaging a commonly used function for repeated use. It's not a general-purpose release, and unless you really hate writing very short bits of requests code, it's likely not worth your trouble.
+0.0.1 - This release is intended primarily to make the developers life slightly
+easier by packaging a commonly used function for repeated use. It's not a
+general-purpose release, and unless you really hate writing very short bits of
+requests code, it's likely not worth your trouble.
 
+0.0.2 - As with version 0.0.1, but with some slightly more sophisticated
+handling of what to do if a data file already exists, avoid accidental
+overwrites or wasted bandwidth from pulling down redundantly huge files. Also
+cleaned up code with Flake8.
 
 Why 'Pumphandle'?
 -----------------
--- pumphandle/__init__.py
+++ pumphandle/__init__.py
@@ -1,30 +1,30 @@
-def NetDrop(url,filename):
-	'''Simple function to check for existence of data file and download file from internet
-	source if not otherwise present'''
-	from requests import get
-	import os
-	fullname = os.path.join(os.getcwd(),filename)
-	if os.path.isfile(fullname):
-		existswarn = raw_input("File already exists. Overwrite (Y/N)? ")
-		existswarn = existswarn.lower()
+def NetDrop(url, filename):
+    '''Simple function to check for existence of data file and download file
+    from internet source if not otherwise present'''
+    from requests import get
+    import os
+    fullname = os.path.join(os.getcwd(), filename)
+    if os.path.isfile(fullname):
+        existswarn = raw_input("File already exists. Overwrite (Y/N)? ")
+        existswarn = existswarn.lower()
 
-		while existswarn not in ['y','n']:
-			print "Invalid input, please try again."
-			existswarn = raw_input("File already exists. Overwrite (Y/N)? ")
-			existswarn = existswarn.lower()
+        while existswarn not in ['y', 'n']:
+            print "Invalid input, please try again."
+            existswarn = raw_input("File already exists. Overwrite (Y/N)? ")
+            existswarn = existswarn.lower()
 
-		if existswarn == "y":
-			netfile = get(url, verify=False)
-			f = open(fullname,'w')
-			f.write(netfile.content)
-			f.close()
-			
-		elif existswarn =="n":
-			print "Download canceled"
-			
-	else:
-		netfile = get(url, verify=False)
-		f = open(fullname,'w')
-		f.write(netfile.content)
-		f.close()
-	return()
+        if existswarn == "y":
+            netfile = get(url, verify=False)
+            f = open(fullname, 'w')
+            f.write(netfile.content)
+            f.close()
+
+        elif existswarn == "n":
+            print "Download canceled"
+
+    else:
+        netfile = get(url, verify=False)
+        f = open(fullname, 'w')
+        f.write(netfile.content)
+        f.close()
+    return()
